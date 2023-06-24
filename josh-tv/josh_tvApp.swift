da3cfbb4d3dc4155f9b068d46a37e70937df18d5
Jeff Sisson <jeff@bigboy.us>
2023-06-24 08:42:16 -0400

    Initial Commit

---
+++ josh-tv/josh_tvApp.swift
@@ -0,0 +1,17 @@
+//
+//  josh_tvApp.swift
+//  josh-tv
+//
+//  Created by Jeffrey Sisson on 6/24/23.
+//
+
+import SwiftUI
+
+@main
+struct josh_tvApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
